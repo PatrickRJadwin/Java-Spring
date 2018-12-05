e5d90aabc1724cbf731655c0c19dbc3ba5d9df73
Patrick Jadwin <prjadwin@pstcc.edu>
2018-12-04 21:40:25 -0500

    Dependency and setter injection implemented

--- src/com/luv2code/springdemo/HockeyCoach.java
+++ src/com/luv2code/springdemo/HockeyCoach.java
@@ -2,7 +2,21 @@ package com.luv2code.springdemo;
 
 public class HockeyCoach implements Coach {
 	
+	// define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	// define a constructor for dependency injection
+	public HockeyCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
+	@Override
 	public String getDailyWorkout() {
 		return "100 Slapshots";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 }
--- src/com/luv2code/springdemo/SetterDemoApp.java
+++ src/com/luv2code/springdemo/SetterDemoApp.java
@@ -2,26 +2,25 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloSpringApp {
+public class SetterDemoApp {
 
 	public static void main(String[] args) {
 		
-		// load the spring configuration
+		// load the spring configuration file
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// retrieve the bean from spring container
-		Coach theCoach = context.getBean("myCoach", Coach.class);
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
-		// call methods on the bean
+		// call methods on bean
+		// .. come back to this ..
 		System.out.println(theCoach.getDailyWorkout());
 		
-		// lets call our new method for fortunes
 		System.out.println(theCoach.getDailyFortune());
 		
 		// close the context
 		context.close();
-		
 	}
 
 }
