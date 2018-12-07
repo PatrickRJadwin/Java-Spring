9f7d7a3830f32736a2599ed2b5981f8790a9674f
Patrick Jadwin <prjadwin@pstcc.edu>
2018-12-06 23:36:21 -0500

    Set scope and generate random fortune

--- src/com/luv2code/springdemo/HappyFortuneService.java
+++ src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,10 +1,15 @@
 package com.luv2code.springdemo;
 
-public class HappyFortuneService implements FortuneService {
+import java.util.Random;
 
+public class HappyFortuneService implements FortuneService {
+	
+	private final String[] fortunes = new String[] {"Today is your lucky day!", "You will meet someone interesting today", "You will meet a goal today"};
+	
 	@Override
 	public String getFortune() {
-		return "Today is your lucky day!";
+		Random r = new Random();
+		return fortunes[r.nextInt(3) + 0];
 	}
 
 }
